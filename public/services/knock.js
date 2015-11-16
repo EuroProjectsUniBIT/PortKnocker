f6d58c2209c6d19b47e9e05b088a67b404ac05c2
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2015-11-16 22:05:34 +0200

    url fix

--- public/services/knock.js
+++ public/services/knock.js
@@ -1,22 +1,24 @@
 'use strict';
 
 angular.module('myApp')
-  .service('KnockRequester', ['$http', function($http) {
+  .service('KnockRequester', ['$http', '$location', function($http, $location) {
     this.sendKnock = function(input) {
 
       var ip = input.ip;
       var port = input.port | null;
 
       if (input.protocol === "icmp") {
-        $http.get('http://localhost:3000/ping?ip=' + ip).then(function(result) {
+        $http.get($location.$$protocol + '://' + $location.$$host + ':' + $location.$$port + '/ping?ip=' + ip).then(function(result) {
           console.log('ICMP request IP: ' + ip + 'PORT:' + port);
+          console.log($location.$$protocol + '://' + $location.$$host + ':' + $location.$$port + '/ping?ip=' + ip);
         });
       }
 
       if (input.protocol === "tcp") {
         // console.log('http://localhost:3000/tcp?ip=' + ip + '&port=' + port);
-        $http.get('http://localhost:3000/tcp?ip=' + ip + '&port=' + port).then(function(result) {
+        $http.get($location.$$protocol + '://' + $location.$$host + ':' + $location.$$port + '/tcp?ip=' + ip + '&port=' + port).then(function(result) {
           console.log('TCP request IP: ' + ip + 'PORT:' + port);
+          console.log($location.$$protocol + '://' + $location.$$host + ':' + $location.$$port + '/ping?ip=' + ip);
         });
       }
 
